577913d5a9a6add99e9efc052962a34d35516385
Saad Sheikh <saadsheikh27@gmail.com>
2020-07-05 12:55:44 +0500

    v6 navigation

---
+++ src/Navigation/MainStackNavigator.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+import SignUpScreen from '../Screens/SignUpScreen'
+import GroupScreen from '../Screens/GroupScreen'
+import AddGroupScreen from '../Screens/AddGroupScreen'
+import ChatScreen from '../Screens/ChatScreen'
+
+
+const Stack = createStackNavigator()
+
+const ChatFlow = () => {
+
+    return (
+
+        <NavigationContainer>
+            <Stack.Navigator name="chat">
+
+                <Stack.Screen
+                    name="SignInScreen"
+                    component={SignUpScreen}
+                    options={{ headerShown: false }}
+                />
+
+                <Stack.Screen
+                    name="GroupScreen"
+                    component={GroupScreen}
+                    options={{ title: "Groups" }}
+                />
+
+                <Stack.Screen
+                    name="Add Group Screen"
+                    component={AddGroupScreen}
+                    options={{ title: "Add Group" }}
+                />
+
+                <Stack.Screen
+                    name="Chat"
+                    component={ChatScreen}
+                    options={{ title: "Chat" }}
+                />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+
+
+
+}
+
+
+const MainStackNavigator=()=>{
+    
+    return(
+
+        ChatFlow()
+    )
+
+}
+
+export default MainStackNavigator;
---
+++ src/Screens/AddGroupScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import {StyleSheet,View,Text} from 'react-native'
+
+
+
+const AddGroupScreen=()=>{
+
+
+    return(
+        <View style={styles.container} >
+                <Text style={styles.text}>Add Group Screen</Text>
+        </View>
+    )
+
+
+
+}
+
+
+const styles=StyleSheet.create({
+
+
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#ebebeb"
+    },
+    text:{
+        color:"#101010",
+        fontSize:25,
+        fontWeight:"bold"
+    }
+
+})
+
+export default AddGroupScreen;
